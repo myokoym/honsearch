8db1f9849b4a4aaf80ecc5cac169e0246bde8b47
Masafumi Yokoyama <yokoyama@clear-code.com>
2019-07-07 16:51:19 +0900

    searcher: increase title and subtitle wait

--- lib/honsearch/groonga_searcher.rb
+++ lib/honsearch/groonga_searcher.rb
@@ -83,8 +83,8 @@ module Honsearch
         end
         unless words.empty?
           match_target = record.match_target do |match_record|
-              (match_record.index('Terms.Books_title') * 10) |
-              (match_record.index('Terms.Books_subtitle')) |
+              (match_record.index('Terms.Books_title') * 100) |
+              (match_record.index('Terms.Books_subtitle') * 10) |
               (match_record.index('Terms.Books_content'))
           end
           full_text_search = words.collect {|word|
